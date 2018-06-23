9dfd801cda776ab9c97819ad0e036a43ec41308d
Marissa Zhong <mwz2101@columbia.edu>
2018-06-23 22:15:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,4 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(n)
   if n % 3 == 0 && n % 5 == 0
+    puts "FizzBuzz"
+  elsif n % 3 == 0
+    puts "Fizz"
+  elsif n % 5 == 0
+    puts "Buzz"
+  end
 end
